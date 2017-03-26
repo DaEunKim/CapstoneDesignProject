565525d9a27f2ef31d33585282d5e6ffeb937039
Yoon <dbsaudtlr04@hanmail.net>
2017-03-26 20:49:59 +0900

    more test set

--- myoung/color_test/label_image.py
+++ myoung/color_test/label_image.py
@@ -26,8 +26,13 @@ with tf.Session() as sess:
     
     # Sort to show labels of first prediction in order of confidence
     top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+
+    count = 0
     
     for node_id in top_k:
         human_string = label_lines[node_id]
         score = predictions[0][node_id]
         print('%s (score = %.5f)' % (human_string, score))
+        count += 1
+        if (count == 5):
+            break
